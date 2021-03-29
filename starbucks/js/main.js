81268989a3b17bba96f1e6c9ddf9a083bc524ddc
jaejin <shzhem764@naver.com>
2021-03-30 00:20:06 +0900

    유튜브 영상삽입

--- starbucks/js/main.js
+++ starbucks/js/main.js
@@ -105,4 +105,33 @@ promotionToggleBtn.addEventListener('click',function(){
 })
 
 
+// 범위 랜덤 함수(소수점 2자리까지)
+function random(min, max) {
+  // `.toFixed()`를 통해 반환된 문자 데이터를,
+  // `parseFloat()`을 통해 소수점을 가지는 숫자 데이터로 변환
+  return parseFloat((Math.random() * (max - min) + min).toFixed(2))
+}
+
+
+function floatingObject(selector,delay,size){
+  // gsap.to(애니메이션요소, 시간, 옵션);
+  gsap.to(
+    selector, //선택자 
+    random(1.5, 2.5), //애니메이션 동작 시간
+    { //옵션
+    // y축을 의미함
+    y : size,
+    // 반복 횟수
+    repeat : -1,  // 무한반복(-1) 
+    yoyo: true,  //한번 재생한거 다시 뒤로 하게하는 것 
+    ease: Power1.easeInOut,   
+    delay: random(0, delay) //몇초 뒤에 애니메이션을 실행 할것인가?
+  });
+}
+
+floatingObject('.floating1', 1, 15);
+floatingObject('.floating2', .5, 15);
+floatingObject('.floating3', 1.5, 20);
+
+
 
